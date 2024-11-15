a602cd65b362db5e42fe1426a0797cc32d74922c
Isuru N Liyanaarachchi <isurunaveen27@gmail.com>
2024-11-15 12:17:02 +0530

    all registration requests page added

--- app/views/pages/admin/allRegistrationRequests.php
+++ app/views/pages/admin/allRegistrationRequests.php
@@ -3,23 +3,55 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-    <link rel="stylesheet" href="../styles/admin/sidebar.css">
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: Arial, sans-serif;
-            display: flex;
-        }
-    </style>
+    <title>Registration requests UI</title>
+    <link rel="stylesheet" href="../../styles/admin/complaints.css">
+    <link rel="stylesheet" href="../../styles/admin/sidebar.css">
+    <script src="../../scripts/admin/complaints.js" defer></script>
 </head>
 <body>
-    <?php include __DIR__ . '/../../components/admin/sidebar.php'; ?>
-    <div>
-        <h1>Single Registration Request page</h1>
-        <p>Here shows registration form of a user</p>
-        <button onclick="window.location.href='/adminviewcontroller/allRegistrationRequests'">Go back to all registration requests view</button>
+    <div class="container">
+        <div id="sidebar-container">
+            <?php include __DIR__ . '/../../components/admin/sidebar.php'; ?>
+        </div>
+        <div class="main-content">
+            <h1>All Registration Requests</h1>
+    
+            <div class="search-container">
+                <input type="text" class="search-bar" placeholder="Search by username or ID">
+            </div>
+    
+            <div class="customer-complaints">
+                <h2>Registration Requests</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>User</th>
+                            <th>Date</th>
+                            <th>Request</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+    
+        </div>
+    
+        <div class="right-sidebar">
+             
+            <div class="filter-header"><span>&#x1F50E; FILTERS</span></div>
+            
+            <div class="filter-section">
+                <div class="filter-title">Sort type</div>
+                <div class="filter-options">
+                    <label><input type="radio" name="sort-type" checked> Newest first</label>
+                    <label><input type="radio" name="sort-type"> Oldest first</label>
+                </div>
+            </div>
+            
+        </div>
     </div>
 </body>
 </html>
