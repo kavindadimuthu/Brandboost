784ad5c8df61e91f4f0fdfa1ff0a47a88393687d
Kavinda Dewmith <kavindadewmith@gmail.com>
2024-11-21 23:58:23 +0530

    register pages corrected and styled

--- app/views/pages/common/registerInfluencer.php
+++ app/views/pages/common/registerInfluencer.php
@@ -1,13 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Influencer Register</title>
+    <title>Register as Influencer</title>
+    <link rel="stylesheet" href="../styles/common/Register.css">
 </head>
+
 <body>
-    <h1>This is Influencer Register Page</h1>
-    <button onclick="window.location.href='/homecontroller/index'">Back to home</button>
-    <button onclick="window.location.href='/homecontroller/login'">Go to login</button>
+    <a href="http://localhost:8000/homecontroller/index">
+        <div class="logo">
+            <img src="../assets/images/sample-logo.png" alt="">
+            <span>Brandboost</span>
+        </div>
+    </a>
+    <div class="register-container">
+        <h1>Register as Influencer</h1>
+
+        <form class="register-form" method="post" action="/RegisterController/register" enctype="multipart/form-data">
+
+            <div class="form-group">
+                <input type="text" name="first_name" placeholder="First Name" required>
+            </div>
+            <div class="form-group">
+                <input type="text" name="last_name" placeholder="Last Name" required>
+            </div>
+
+            <div class="form-group">
+                <input type="email" name="email" placeholder="E-mail" required>
+            </div>
+
+            <div class="form-group">
+                <input type="tel" name="phone_number" placeholder="Phone Number" required>
+            </div>
+            
+            <div class="form-group">
+                <input type="text" name="social_media_link" placeholder="Social media link" required>
+            </div>
+
+            <div class="form-group">
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+            <div class="form-group">
+                <input type="password" name="confirm_password" placeholder="Confirm Password" required>
+            </div>
+
+            <div class="form-group-tick">
+                <input type="checkbox" id="privacy" name="privacy_policy" required>
+                <label for="privacy">I agree to all statements in <a href="#">Privacy Policy for
+                        BRANDBOOST</a></label>
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn">Register</button>
+            </div>
+            <p>Already registered? <a href="#">Login here</a></p>
+
+        </form>
+
+    </div>
+
 </body>
+
 </html>
