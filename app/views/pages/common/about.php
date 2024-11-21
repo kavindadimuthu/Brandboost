30271d26cdce8aadad07a9b0416afe96a47c6036
Kavinda Dewmith <kavindadewmith@gmail.com>
2024-11-21 19:38:31 +0530

    database sql file added

--- app/views/pages/common/about.php
+++ app/views/pages/common/about.php
@@ -3,11 +3,130 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
+    <title>Brandboost | About Us</title>
+    <link rel="stylesheet" href="../../styles/common/About.css">
 </head>
 <body>
-    <h1>About Us Page</h1>
-    <p>This is about page.</p>
-    <a href="http://localhost:8000/homecontroller/index">home page</a>
+    <header>
+        <div class="wrapper">
+            <div class="logo header-logo">
+                <img src="../../assets/images/sample-logo.png" alt="">
+                <span>Brandboost</span>
+            </div>
+            <nav class="nav">
+                <ul class="nav_list">
+                    <li class="nav_list_item"><a href="#">Services</a></li>
+                    <li class="nav_list_item"><a href="#">About Us</a></li>
+                    <li class="nav_list_item"><a href="#">Contact Us</a></li>
+                    <li class="nav_list_item"><a href="#">FAQs</a></li>
+                </ul>
+            </nav>
+            <div class="log-reg">
+                <button class="btn login-btn">Login</button>
+                <button class="btn register-btn">Sign Up</button>
+            </div>
+        </div>
+    </header>
+    
+    <section class="aboutus">
+        <div class="wrapper">
+            <div class="aboutus-content">
+                <div class="heading">About Us</div>
+                <div class="description">
+                        Welcome to Brandboost, the ultimate destination where creativity meets opportunity!<br><br>
+                        Our platform bridges the gap between influencers, designers, and businesses, creating a seamless marketplace for promoting and designing services. We believe in empowering individuals and organizations by fostering meaningful collaborations that drive growth, creativity, and success.
+                </div>
+            </div>
+            <div class="aboutus-image">
+                <img src="../../assets/images/about/aboutus-main-group.png" alt="">
+        </div>
+    </section>
+
+    <section class="our-values">
+        <div class="wrapper">
+            <div class="heading">Our Values</div>
+            <div class="values-container">
+                <div class="value-card">
+                    <img src="../../assets/images/about/Image 62 (1).png" alt="">
+                    <h3>Diverse Talent Pool</h3>
+                    <div class="description">Access a curated network of skilled influencers and designers</div>
+                </div>
+                <div class="value-card">
+                    <img src="../../assets/images/about/Image 60.png" alt="">
+                    <h3>Transparent Process</h3>
+                    <div class="description">Enjoy clear pricing, secure payments, and straightforward communication</div>
+                </div>
+                <div class="value-card">
+                    <img src="../../assets/images/about/Image 61.png" alt="">
+                    <h3>Growth-Focused</h3>
+                    <div class="description">We’re here to help you grow, whether you’re building your brand or expanding your portfolio</div>
+                </div>
+        </div>
+    </section>
+    
+    <section class="our-mission">    
+        <div class="wrapper">
+            <div class="heading">Our Mission</div>
+            <p>Our mission is to create a thriving ecosystem where businesses, influencers, and designers come together to achieve mutual success. We are committed to fostering innovation, collaboration, and trust in every interaction.</p>
+        </div>
+    </section>
+    
+
+    <footer class="footer">
+        <div class="wrapper">
+            <div class="footer-upper-container">
+                <div class="footer-section">
+                    <div class="logo footer-logo">
+                        <img src="../../assets/images/sample-logo.png" alt="">
+                        <span>Brandboost</span>
+                    </div>
+                </div>
+                <div class="footer-section">
+                    <h3>Resources</h3>
+                    <ul>
+                      <li><a href="#">Blog</a></li>
+                      <li><a href="#">User guides</a></li>
+                      <li><a href="#">Webinars</a></li>
+                    </ul>
+                  </div>
+                  <div class="footer-section">
+                    <h3>Community</h3>
+                    <ul>
+                      <li><a href="#">Developers</a></li>
+                      <li><a href="#">Users</a></li>
+                    </ul>
+                  </div>
+                  <div class="footer-section">
+                    <h3>Company</h3>
+                    <ul>
+                      <li><a href="#">About</a></li>
+                      <li><a href="#">Join us</a></li>
+                    </ul>
+                  </div>
+                  <div class="footer-section">
+                    <h3>Support</h3>
+                    <ul>
+                      <li><a href="#">Help center</a></li>
+                      <li><a href="#">Chat support</a></li>
+                    </ul>
+                  </div>
+            </div>
+            <div class="footer-lower-container">
+                <div class="language-selection">
+                    <select name="language" id="language">
+                        <option value="English">English</option>
+                        <option value="Sinhala">Sinhala</option>
+                    </select>
+                </div>
+                <div class="copyright">© 2022 Brand, Inc. Privacy Terms Sitemap</div>
+                <div class="social-media-links">
+                    <img src="../../assets/icons/social-media/icons8-twitterx.svg" alt="">
+                    <img src="../../assets/icons/social-media/icons8-facebook.svg" alt="">
+                    <img src="../../assets/icons/social-media/icons8-instagram.svg" alt="">
+                    <img src="../../assets/icons/social-media/icons8-youtube.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
