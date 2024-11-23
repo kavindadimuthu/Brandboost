c5c5c2e5ad2e1f5b568b25e2121ebd9a30dbe300
Kasunthara <kasuntharamarasinghe@gmail.com>
2024-11-23 11:36:33 +0530

    common registration edited

--- app/views/pages/common/register.php
+++ app/views/pages/common/register.php
@@ -4,13 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+    <link rel="stylesheet" href="../../styles/common/commonregister.css">
 </head>
 <body>
-    <h1>This is Register Selection Page</h1>
-    <button onclick="window.location.href='/homecontroller/registerBusiness'">Register as Business</button>
-    <button onclick="window.location.href='/homecontroller/registerInfluencer'">Register as Influencer</button>
-    <button onclick="window.location.href='/homecontroller/registerDesigner'">Register as Designer</button>
-    <button onclick="window.location.href='/homecontroller/index'">Back to home</button>
-    <button onclick="window.location.href='/homecontroller/login'">Go to login</button>
+    <div class="navbar">
+        <button class="nav-button" onclick="window.location.href='/homecontroller/index'">Back</button>
+        <span class="logo">Brandboost</span>
+        <button class="nav-button" onclick="window.location.href='/homecontroller/login'">Login</button>
+    </div>
+    
+<div class="container">   
+        <h1>Register As</h1>
+        <div class="container">
+            <button class="register-btn" onclick="window.location.href='/homecontroller/registerBusiness'">Business</button>
+            <button class="register-btn" onclick="window.location.href='/homecontroller/registerInfluencer'">Influencer</button>
+            <button class="register-btn" onclick="window.location.href='/homecontroller/registerDesigner'">Designer</button>
+            </div>
+        <script src="script.js"></script>
+</div>
 </body>
 </html>
